bc10892c2ce159e33fb1313c4cfe7183a5ff179c
Yer Thao <maivyerthao@gmail.com>
2021-01-09 15:59:44 -0600

    Reviewed react input, form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,42 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+
+// creating local state to store variables
+state = {
+  name: '',
+  last: ''
+}
+
+handleChange = (event, input) => {
+  this.setState({
+    [input]: event.target.value
+  })
+}
+
+handleClick = () => {
+  console.log('Submit button working');
+  this.setState({
+    name: '',
+    last: ''
+  })
+}
+
+
   render(){
+    console.log('Rendering:', this.state);
+    
     return(
       <div>
         <header>
-          <h1>Spike Solo Project</h1>
+          <h1>{this.state.name} {this.state.last}'s Spike Solo Project</h1>
+          <form>
+            <label>Name:</label>
+            <input type="text" value={this.state.name} onChange={(event) => this.handleChange(event, 'name')}/>
+            <label>Last:</label>
+            <input type="text" value={this.state.last} onChange={(event) => this.handleChange(event, 'last')}/>
+            <button onClick={this.handleClick}>Submit</button>
+          </form>
         </header>
       </div>
     )
